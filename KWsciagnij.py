7c1f3714720b2bf917d1ed425a04da1e1fe745fb
Daniel Tulaj <s3rek92@gmail.com>
2020-03-18 20:21:07 +0100

    working

--- KWsciagnij.py
+++ KWsciagnij.py
@@ -5,62 +5,79 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 import codecs
 import tkinter
+import subprocess
+import os
 
 
-def GetNumerKW():
+def GetTXTfile():
     TXTpath=r'C:\Users\Daniel\Documents\programy\KWpython\LaziskabezWlodka.txt'
-    TXTfile=open(TXTpath,'r')
-    for NumerKW in TXTfile:
-        print(NumerKW)
-        return NumerKW
-
-GetNumerKW()
+    TXTFile=open(TXTpath,'r')
+    return TXTFile
 
 def CreateDriver():
+    
+    #acessing Chrome user profile
     chrome_options = Options()
     chrome_options.add_argument("profile-directory=Profile 1")
     chrome_options.add_argument(r"user-data-dir=C:\Users\Daniel\AppData\Local\Google\Chrome\User Data")
 
-
-def tst():
-    chrome_options = Options()
-    chrome_options.add_argument("profile-directory=Profile 1")
-    chrome_options.add_argument(r"user-data-dir=C:\Users\Daniel\AppData\Local\Google\Chrome\User Data")
-
+    #Creating chrome webdriver and opening page
     driver=webdriver.Chrome(executable_path=r'C:\Users\Daniel\Documents\programy\KWpython\.vscode\chrome\chromedriver.exe',chrome_options=chrome_options)
     driver.get("https://przegladarka-ekw.ms.gov.pl/eukw_prz/KsiegiWieczyste/wyszukiwanieKW?komunikaty=true&kontakt=true&okienkoSerwisowe=false")
+    return driver
+  
+#input KW data
+def InputKWdata(NumerKW):
+    SpltdKW=NumerKW.split("_")
     kodWydzialuInput = driver.find_element_by_id("kodWydzialuInput")
-
-
-    kodWydzialuInput.send_keys("GL1W")
+    kodWydzialuInput.send_keys(SpltdKW[0])
     numerKsiegiWieczystej = driver.find_element_by_id('numerKsiegiWieczystej')
-    numerKsiegiWieczystej.send_keys('00012278')
+    numerKsiegiWieczystej.send_keys(SpltdKW[1])
     cyfraKontrolna= driver.find_element_by_id('cyfraKontrolna')
-    cyfraKontrolna.send_keys('7')
+    cyfraKontrolna.send_keys(SpltdKW[2])
 
-
-
-    wait = WebDriverWait(driver, 200)
-    #Anticaptcha= driver.find_element_by_class_name('antigate_solver recaptcha error')
+#wait for recaptcha tick img to show up
+def Anticaptcha():
+    wait = WebDriverWait(driver, 2000)
     element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'solved_flag')))
+    return print("captcha Solved")
 
+#saves page source to Html file in download folder
+def SaveHtml(filename, index):
+    MyDocpath=os.path.join(os.path.expanduser('~/Documents'),filename+str(index))
+    file_object = codecs.open(MyDocpath +".htm", "w", "utf-8")
+    html = driver.page_source
+    file_object.write(html)
+
+#Clicks given dzial link
+def NextDzial(dzial):
+    DzialLink=driver.find_element_by_xpath("//input[@value='"+dzial+"']")
+    DzialLink.click()
+
+#Iterates and saves Html from each dzial
+def IterateDzialy(NumerKW):
+    DzialyList= ["Dział I-O", "Dział I-Sp", "Dział II", "Dział III", "Dział IV"]
+    i=0
+    for dzial in DzialyList:
+        i+=1
+        NextDzial(dzial)
+        #spltedzial=dzial.split(" ")
+        #SaveHtml(file=spltedzial[1])
+        SaveHtml(filename=NumerKW, index=i)
+        
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Main Logic!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+TXTFile=GetTXTfile()
+for NumerKW in TXTFile:
+    NumerKW=NumerKW[:-1]
+    print(NumerKW)
+    driver=CreateDriver()
+    InputKWdata(NumerKW)
+    Anticaptcha()
     #CLick wyszukaj
     driver.find_element_by_id('wyszukaj').click()
-
     #click wydrukZupelny
     driver.find_element_by_id('przyciskWydrukZupelny').click()
-
-    #Dział III Dział II Dział I-Sp Dział I-O Dział IV
-    file_object = codecs.open("dzial0"+"plik.html", "w", "utf-8")
-    html = driver.page_source
-    file_object.write(html)
-
-    dzial=driver.find_element_by_xpath("//input[@value='Dział IV']")
-    dzial.click()
-    file_object = codecs.open("dzial4"+"plik.html", "w", "utf-8")
-    html = driver.page_source
-    file_object.write(html)
-
-
-    print("end")
+    SaveHtml(filename=NumerKW, index="0")
+    IterateDzialy(NumerKW)
     driver.close()
+    subprocess.call(["Htm2pdf.exe",NumerKW])
