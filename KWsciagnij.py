348baa0bb0a21d94a1e812079ca293eb7806403e
Daniel Tulaj <s3rek92@gmail.com>
2020-03-20 17:36:33 +0100

    working and stable version
    needs refactoring

--- KWsciagnij.py
+++ KWsciagnij.py
@@ -6,7 +6,8 @@ from selenium.webdriver.common.by import By
 import codecs
 import tkinter
 import subprocess
-import os
+import os, sys
+from selenium.common.exceptions import NoSuchElementException 
 
 
 def GetTXTfile():
@@ -35,12 +36,40 @@ def InputKWdata(NumerKW):
     numerKsiegiWieczystej.send_keys(SpltdKW[1])
     cyfraKontrolna= driver.find_element_by_id('cyfraKontrolna')
     cyfraKontrolna.send_keys(SpltdKW[2])
-
 #wait for recaptcha tick img to show up
 def Anticaptcha():
-    wait = WebDriverWait(driver, 2000)
-    element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'solved_flag')))
-    return print("captcha Solved")
+    try:
+        wait = WebDriverWait(driver, 20000000000)
+        #wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'solved_flag')))
+        wait.until(EC.text_to_be_present_in_element((By.CLASS_NAME, 'status'), 'Solved'))
+        #driver.implicitly_wait(100000000000)
+    finally:
+        #driver.implicitly_wait(100000000000)
+            driver.find_element_by_id('wyszukaj').click()
+            bledKW=bledneKW()
+            if bledKW==True:
+                sys.exit("bledne KW") 
+            bledKW=bledneKW2()
+            if bledKW==True:
+                sys.exit("bledne KW") 
+
+
+def bledneKW():
+    try:
+        driver.find_element_by_css_selector('div.error')
+    except NoSuchElementException:
+        return False
+    return True
+
+def bledneKW2():
+    try:
+        driver.find_element_by_css_selector('span#cyfraKontrolna--cyfra-kontrolna.error.visible')
+    except NoSuchElementException:
+        return False
+    return True
+
+                
+
 
 #saves page source to Html file in download folder
 def SaveHtml(filename, index):
@@ -64,20 +93,56 @@ def IterateDzialy(NumerKW):
         #spltedzial=dzial.split(" ")
         #SaveHtml(file=spltedzial[1])
         SaveHtml(filename=NumerKW, index=i)
-        
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Main Logic!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-TXTFile=GetTXTfile()
-for NumerKW in TXTFile:
-    NumerKW=NumerKW[:-1]
-    print(NumerKW)
-    driver=CreateDriver()
-    InputKWdata(NumerKW)
-    Anticaptcha()
-    #CLick wyszukaj
-    driver.find_element_by_id('wyszukaj').click()
-    #click wydrukZupelny
-    driver.find_element_by_id('przyciskWydrukZupelny').click()
+
+def rest():
+    #driver.find_element_by_id('przyciskWydrukZupelny').click()
     SaveHtml(filename=NumerKW, index="0")
     IterateDzialy(NumerKW)
     driver.close()
     subprocess.call(["Htm2pdf.exe",NumerKW])
+
+def tst2(driver):
+    InputKWdata(NumerKW)
+    Anticaptcha()
+    #click wydrukZupelny
+    #wait = WebDriverWait(driver, 20000000)
+    #while wait.until_not(EC.presence_of_element_located((By.CLASS_NAME, 'solved_flag'))):
+    #    tst2()
+    #wait.until(EC.text_to_be_present_in_element((By.CLASS_NAME, 'status'), 'Solved'))
+    """while EC.text_to_be_present_in_element((By.CLASS_NAME, 'status'), 'Solved')==False:
+        Anticaptcha()
+    else:"""
+    try:
+        driver.find_element_by_id('przyciskWydrukZupelny').click()
+    except NoSuchElementException:
+        tst2(driver)
+
+    """"while wyn==False:
+        try:
+            driver.find_element_by_id('przyciskWydrukZupelny').click()
+        except NoSuchElementException:
+            tst2()
+            wyn=False
+        wyn=True"""
+    #driver.find_element_by_id('wyszukaj').click()
+    """try:
+        wait.until(EC.element_to_be_clickable((By.ID, 'przyciskWydrukZupelny')))
+    finally:"""
+    #driver.find_element_by_id('przyciskWydrukZupelny').click()
+
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Main Logic!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+TXTFile=GetTXTfile()
+for NumerKW in TXTFile:
+    wyn=False
+    NumerKW=NumerKW[:-1]
+    print(NumerKW)
+    driver=CreateDriver()
+    tst2(driver)
+
+
+    SaveHtml(filename=NumerKW, index="0")
+    IterateDzialy(NumerKW)
+    subprocess.call(["Htm2pdf.exe",NumerKW])
+    driver.close()
